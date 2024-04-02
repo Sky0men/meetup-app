212f4a35e8d64ed6f77ba15183082f80b864c4f5
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2024-04-02 13:15:41 +0300

    создание хедера для приложения

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  constructor(public authService: AuthService) {}
+
+
+  public removeToken() {
+     this.authService.logout()
+   }
+}
